fc7754f6ad471fab112a35a3b601d1298bb8da4a
Andre Sudi Yudatama <andresudi@Andres-MacBook-Pro.local>
2018-07-14 00:13:25 +0700

    Sudoku

--- sudoku.js
+++ sudoku.js
@@ -1,12 +1,62 @@
 "use strict"
 
 class Sudoku {
-  constructor(board_string) {}
+  constructor(board_string) {
+    this.string = Number(board_string)
+    this.sudokuBoard = this.boardArray(board_string)
+    this.emptyCoordinate = this.emptyCoordinate(this.sudokuBoard)
+  }
+
+  boardArray(string) {
+    let outputBoard = [] 
+    let filledBoard = []
+
+    for (let i in string) {
+      filledBoard.push(string[i])
+      if (filledBoard.length == 9) {
+        outputBoard.push(filledBoard)
+        filledBoard = []
+      }
+    }
+    return outputBoard
+  }
+
+  emptyCoordinate(sudokuBoard) {
+    let emptyPosition = []
+    for (let i in sudokuBoard) {
+      for (let j in sudokuBoard[i]) { 
+        if (sudokuBoard[i][j] == 0) {
+          emptyPosition.push([i,j])
+        }
+      }
+    }
+    return emptyPosition
+  }
+
+  checkRow(sudokuBoard, row, value) {
+    for (let i in sudokuBoard[row]) { // i < 9
+      if (sudokuBoard[row][i] === value) {
+        return false
+      }
+    }
+    return true
+  }
+
+  checkColumn (sudokuBoard, col, value) {
+    for (let i in sudokuBoard) {
+      if (sudokuBoard[i][col] === value) {
+        return false
+      }
+    }
+    return true
+  }
 
   solve() {}
 
   // Returns a string representing the current state of the board
-  board() {}
+  board() {
+
+  }
 }
 
 // The file has newlines at the end of each line,
@@ -21,4 +71,10 @@ var game = new Sudoku(board_string)
 // Remember: this will just fill out what it can and not "guess"
 game.solve()
 
-console.log(game.board())
+console.log(`===================Sudoku board==========`)
+console.log(game.boardArray(board_string))
+console.log(`=========+Empty Coordinates================`)
+console.log(game.emptyCoordinate)
+console.log(game.checkRow(board_string, 0, 1))
+//console.log(game.sudokuBoard)
+//console.log(game.board())
