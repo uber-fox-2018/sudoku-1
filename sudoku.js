497ffea87d95b376d0ee49308131213d8819d7ea
Harles Bayu Anggara <bayuanggaraharles@gmail.com>
2018-07-14 17:02:38 +0700

    horizontal, vertikal, box 3x3, next to backtrac

--- sudoku.js
+++ sudoku.js
@@ -18,20 +18,18 @@ class Sudoku {
       let trigger = false
       
       while(!trigger && value <=9){
-        if(this.checkHorizontal(posisiKosong[i],value,initBoard) && this.checkVertikal(posisiKosong[i],value,initBoard)){
+        if(this.allMatchNumber(posisiKosong[i],value,initBoard)){
           initBoard[posX][posY] = value
           trigger = true
         } else{
           value++
         }
       }
-
     }
 
     return initBoard
 
   }
-
 
   checkHorizontal(posisi,value,initBoard){
 
@@ -53,10 +51,23 @@ class Sudoku {
     return true
   }
 
-  allMatchNumber(){
-
+  checkBox(posisi,value,initBoard){
+    for(let i = posisi[0] - (posisi[0] % 3); i < 3; i++){
+      for(let j = posisi[1] - (posisi[1] % 3); j < 3; j++){
+        if(initBoard[i][j] === value){
+          return false
+        }
+      }
+    }
+    return true
   }
 
+  allMatchNumber(posisi,value,initBoard){
+    if(this.checkHorizontal(posisi,value,initBoard) && this.checkVertikal(posisi,value,initBoard) && this.checkBox(posisi,value,initBoard)){
+      return true
+    }
+    return false
+  }
 
   // Returns a string representing the current state of the board
   board() {
@@ -106,4 +117,5 @@ var game = new Sudoku(board_string)
 // Remember: this will just fill out what it can and not "guess"
 // game.solve()
 console.log(game.solve())
-// console.log(game.board())
+console.log('=========================')
+console.log(game.board())
