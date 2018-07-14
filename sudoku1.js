98a33b57ee0024baed5449e2fc61ccd29aa2a30b
ridhoemgl <ridhoemgl@gmail.com>
2018-07-14 17:13:21 +0700

    vertical and horizontal fixed

--- sudoku1.js
+++ sudoku1.js
@@ -1,4 +1,4 @@
-"use strict"
+          "use strict"
 
 const row = 9
 const col = 9
@@ -6,7 +6,6 @@ const col = 9
 class Sudoku {
   constructor(board_string) {
 
-  	// this.board = board(arr)
   }
 
   solve() { 
@@ -15,14 +14,13 @@ class Sudoku {
   	function master (arr, row, col){
   		let numString='123456789'
   		right(arr,row,col)
-  		function right(arrRight,rowRight,colRight){
-  			// console.log(papan)
 
+
+  		function right(arrRight,rowRight,colRight){
   			let tempNum = []
   			for (let a = 0 ; a < numString.length ; a++){
   				let countR=0
   				for (let b = 0 ; b < arr.length ; b++){
-  					// console.log(a,b)
 	  				if (numString[a] !== arr[b] ){
 	  					countR++
 	  				}
@@ -30,100 +28,74 @@ class Sudoku {
   				if (countR === arr.length){
   					tempNum.push(numString[a])
   				}
-
-  				// break
   			}
-  			// console.log('temp',tempNum)
-
   			bottom(tempNum,rowRight,colRight)
   		}
 
   		function bottom(arrBottom,rowBottom,colBottom){
-
   			for (let a = 0 ; a < arrBottom.length ; a++){
   				for (let b = 0 ; b < arr.length ; b++){
   					if (arrBottom[a] === papan[b][colBottom]){
   						arrBottom.splice(a,1)
   						a-=1
   						b-=1
-  						// console.log('arrBottom',arrBottom)
   					}
-  			
   				}
-  				// for (let b = 0 ; b < )
-  				
   			}
-  			// console.log('arrBottom',arrBottom)
-  			// console.log(papan)
+        
+        /*
+          ==============================
+          HORIZONTAL & VERTICAL MATRIKS FIXED
+          - UNCOMMENT BELLOW TO CHECK IT & COMMENT SQUARE FUNCTION
+          ==============================
+                        |
+                        |
+        */
+
+        // console.log(arrBottom)
+        // if (arrBottom.length !== 0){
+        //   papan[rowBottom][colBottom] = arrBottom[0]
+        // }
+        // console.log(papan)
+
   			square(arrBottom, rowBottom, colBottom)
   		}
 
   		function square(arrSquare, rowSquare, colSquare){
-  			// for (let a = 0 ; a < )
-  			// console.log('row',rowSquare)
-  			// console.log('col',rowSquare)
-
-  			// console.log(papan)
-  			// console.log(papan[0][0])
-  			// console.log(papan[0][1])
-  			// console.log(papan[0][2])
-  			// console.log(papan[1][0])
-  			// console.log(papan[1][1])
-  			// console.log(papan[1][2])
-  			// console.log(papan[2][0])
-  			// console.log(papan[2][1])
-  			// console.log(papan[2][2])
-  			// console.log(papan.join(' '))
-  			// let countS=0
-
+        console.log('rowSquare',rowSquare)
+        console.log('colSquare',colSquare)
+        console.log(papan)
+        console.log(arrSquare)
+        console.log('\n')
+        for (let i = 0 ; i < papan.length ; i++){
+          for (let j = 0 ; j < papan[i].length ; j++){
+            for (let k = 0 ; k < k < arrSquare.length ; k++){
+              
+            }
+          }
+        }
 
   		}
-
-
-  		// return arr
+  		return arr
   	}
   	
-  	
-
-  	
-  	let batasRow = 0
-	let batasCol = 0
-
   	for (let i = 0 ; i < papan.length ; i++){
-  		
   		for (let j = 0 ; j < papan[i].length ; j++){
   			if (papan[i][j] === ' '){
-  			console.log('i,j:',i,j)
+  				master(papan[i], i , j)
 
-  				if (i >= 0 && i < 3 && j >= 0 && j < 3){
-  					batasRow = 2
-  					batasCol = 2
-  				}else if (i >= 3 && i < 6 && j >= 3 && j < 6){
-  					batasRow = 5
-  					batasCol = 5
-  				}else if (i >= 6 && i < 9 && j >= 3 && j < 9){
-  					batasRow = 8
-  					batasCol = 8
-  				}
-  				console.log('batasRow:',batasRow)
-  				console.log('batasCol:',batasCol)
-  				master(papan[i], batasRow , batasCol)
-  			}
-  			
-  		}
-  		// break
+  			}	
+      }
+        // console.log(papan)
   	}
-  	// break
   	// return
 
   }
   
-
   // Returns a string representing the current state of the board
   board() {
   	let arr=[]
   	let count =0
-  	// console.log(papan)
   	for (let i = 0 ; i < col ; i++){
   		let temp=[]
   		for (let j = 0 ; j< row ; j++){
@@ -135,9 +107,7 @@ class Sudoku {
   			}
   			count++
   		}
-  		console.log('\n')
   		arr.push(temp)
-  		// console.log(arr)
   	}
   	return arr
   }
